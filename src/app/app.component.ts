454849f4ebc069bf7f3e8052139854f2d88db50d
rsantoyo-dev <ricardo.santoyo@hotmail.com>
2021-11-22 14:07:23 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,16 @@ import { SJss } from 'super-jss/lib/super-jss-model';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  header:SJss = {padding: '3rem', backgroundColor: 'red', color:'white'}
+  header: SJss = {
+    backgroundColor: 'rgb(22 175 161)',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: '3rem',
+  };
 }
